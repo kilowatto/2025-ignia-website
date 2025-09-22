fac665afcf65eae26f78d9ac2376591ddb32811e
Esteban Rey <16902532+kilowatto@users.noreply.github.com>
2025-09-22 13:58:28 -0600

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import ogCanvas from 'astro-og-canvas';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [sitemap(), ogCanvas()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
